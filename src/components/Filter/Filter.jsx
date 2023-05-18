8a9320e59964646dc0731b9a95276b6fc993efa2
Yevhenii2022 <3starosta3@gmail.com>
2023-05-18 18:47:02 +0300

    Updates filter with hooks

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,45 +1,42 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { TextField } from '@mui/material';
 import PersonSearchIcon from '@mui/icons-material/PersonSearch';
 import InputAdornment from '@mui/material/InputAdornment';
 
-export class Filter extends Component {
-  static propTypes = {
-    setFilter: PropTypes.func.isRequired,
-  };
-  state = {
-    value: '',
+export const Filter = ({ setFilter }) => {
+  const [value, setValue] = useState('');
+
+  const handleInputChange = ({ target }) => {
+    setValue(target.value);
+    setFilter(target.value.toLowerCase().trim());
   };
 
-  handleInputChange = ({ target }) => {
-    this.setState({ value: target.value });
-    this.props.setFilter(target.value.toLowerCase().trim());
-  };
+  return (
+    <div>
+      <TextField
+        style={{
+          width: 300,
+        }}
+        variant="outlined"
+        type="text"
+        value={value}
+        onChange={handleInputChange}
+        InputLabelProps={{ shrink: true }}
+        placeholder="find a contact by name"
+        aria-describedby="find a contact by name"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <PersonSearchIcon />
+            </InputAdornment>
+          ),
+        }}
+      />
+    </div>
+  );
+};
 
-  render() {
-    return (
-      <div>
-        <TextField
-          style={{
-            width: 300,
-          }}
-          variant="outlined"
-          type="text"
-          value={this.state.value}
-          onChange={this.handleInputChange}
-          InputLabelProps={{ shrink: true }}
-          placeholder="find a contact by name"
-          aria-describedby="find a contact by name"
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <PersonSearchIcon />
-              </InputAdornment>
-            ),
-          }}
-        />
-      </div>
-    );
-  }
-}
+Filter.propTypes = {
+  setFilter: PropTypes.func.isRequired,
+};
